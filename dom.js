393e326f2d41861e5e7cd7d298fa9fb677bad1c4
joncancode <jc.haines19@gmail.com>
2017-06-15 15:16:02 -0400

    did second problem

--- dom.js
+++ dom.js
@@ -1,27 +1,29 @@
-// $(document).ready(function()({
+$(document).ready(function() {
  
-
-
 //image should be displayed in the 
 //full size image container at the top
 
 //remove the big picture for thumbnail 
-$('.thumbnails img').click(function() {
-    var thmb = this;
-    var src = this.src;
-    $('.hero img').fadeOut(400,function(){
-        thmb.src = this.src;
-        $(this).fadeIn(400)[0].src = src;
-    });
-});
-
-// $('.thumbs img').click(function() {
+// $('.thumbnails img').click(function() {
 //     var thmb = this;
 //     var src = this.src;
-//     $('.bottlesWrapper img').fadeOut(400,function(){
+//     $('.hero img').fadeOut(400,function(){
 //         thmb.src = this.src;
 //         $(this).fadeIn(400)[0].src = src;
 //     });
 // });
+
+//=================================start of problem 2
+
+
+// $('.js-lightbulb').click(function(event){
+//     $('.js-lightbulb').not(event.currentTarget).removeClass('bulb-on')
+//     $(event.currentTarget).toggleClass('bulb-on')
+  
+//    })
+ 
+// })
+
+//=================================start of problem 3
 
 
